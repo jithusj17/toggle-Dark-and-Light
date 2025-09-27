30c02467ad98877a3b59871cfb8ce4ac0bd6b5ca
Jithu SJ <jithusj169@gmail.com>
2025-09-27 21:31:27 +0530

    added the usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import { useState } from "react"
 import Navbar from "./components/navbar/navbar"
 
 function App() {
  
+  const[theme,setTheme] = useState('light')
 
   return (
     <div className="container">
-      <Navbar />
+      <Navbar theme={theme} setTheme={setTheme}/>
     </div>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,10 +8,16 @@ import toogle_light  from "../../assets/night.png"
 import toogle_dark  from "../../assets/day.png"
 
 
-const Navbar = () => {
+const Navbar = ({theme,setTheme}) => {
+    
+    const toggle_mode = ()=>{
+        theme == "light" ? setTheme('dark'): setTheme('light')
+    }
+
+
   return (
     <div className='navbar'>
-      <img src={logo_light} alt='' className='logo' />
+      <img src={theme == "light" ? logo_light: logo_dark} alt='' className='logo' />
       <ul>
       <li>Home</li>
       <li>Products</li>
@@ -20,10 +26,11 @@ const Navbar = () => {
       </ul>
       <div className='search-box'>
         <input type='text' placeholder='Search'/>
-        <img src={search_icon_light} alt='' />
+        <img src={theme == "light" ? search_icon_light:search_icon_dark} alt='' />
 
       </div>
-      <img src={toogle_light} alt='' className='toggle-icon' />
+      <img src={theme == "light" ? toogle_light:toogle_dark} alt='' className='toggle-icon'
+      onClick={()=>{toggle_mode()}} />
     </div>
   )
 }

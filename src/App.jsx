ad08881f6afffb2bf33f8da4cc3a5a0c1d76c4d4
Jithu SJ <jithusj169@gmail.com>
2025-09-28 17:22:14 +0530

    COMPLETED

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Navbar from "./components/navbar/navbar"
 
 function App() {
  
-  const[theme,setTheme] = useState('light')
+
+  const current_theme = localStorage.getItem('current_theme')
+  const[theme,setTheme] = useState(current_theme ? current_theme:'light')
+  useEffect(()=>{
+    localStorage.setItem('current_theme',theme)
+  },[theme])
 
   return (
     <div className={`container ${theme}`}>
       <Navbar theme={theme} setTheme={setTheme}/>
     </div>
   )
-}
+} 
 
 export default App
